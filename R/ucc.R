77bef492ea3337fea01a1acc9b39a8678c893287
Jack Maney <jackmaney@gmail.com>
2013-06-26 15:20:14 -0500

    Initial commit

---
+++ R/ucc.R
@@ -0,0 +1,92 @@
+ucc.is_valid <- function(df){
+  if(!is.data.frame(df))
+  {
+    return(FALSE);
+  }
+  
+  if(ncol(df) != 2)
+  {
+    return(FALSE);
+  }
+  
+  if(!(
+      (typeof(df[,1]) %in% c("numeric","integer","double"))
+       &&
+      (typeof(df[,2]) %in% c("numeric","integer","double"))
+      )
+  ){
+    return(FALSE);
+  }
+  
+  if( (any(is.na(df[,1]))) || (any(is.na(df[,2]))) )
+  {
+    return(FALSE);
+  }
+  
+  return(TRUE);
+}
+
+ucc.sort <- function(df,dependent = 2){
+  
+  if(!(ucc.is_valid(df)))
+  {
+    stop("Provided data frame does not pass validation (exactly two numeric columns with no NA values)");
+  }
+  
+  if(dependent != 1 && dependent != 2)
+  {
+    stop("Invalid value for dependent (1 or 2 only)");
+  }
+  
+  independent = ifelse(dependent == 2,1,2);
+  
+  return(as.data.frame(df[order(df[,independent],df[,dependent]),]));
+ 
+}
+
+ucc.ranks <- function(df,dependent = 2)
+{
+  result <- ucc.sort(df,dependent = dependent)
+  result[,dependent] = rank(result[,dependent],ties.method="random")
+  
+  return(result)
+}
+
+ucc.delta <- function(df,dependent = 2)
+{
+  
+  result = c()
+  
+  for(i in 1:(nrow(df) - 1))
+  {
+    result = c(result,abs(df[i+1,dependent] - df[i,dependent]))
+  }
+  
+  return(result);
+  
+}
+
+ucc.y <- function(df)
+{
+  r <- ucc.ranks(df,2)
+  d <- ucc.delta(r,2)
+  return(1 - (3 * mean(d))/(nrow(r) + 1));
+}
+
+ucc.x <- function(df)
+{
+  r <- ucc.ranks(df,1)
+  d <- ucc.delta(r,1)
+  return(1 - (3 * mean(d))/(nrow(r) + 1));
+}
+
+ucc.ucc <- function(df)
+{
+  ux <- ucc.x(df)
+  uy <- ucc.y(df)
+  u <- max(c(ux,uy))
+  
+  nr = nrow(df)
+  
+  return(list(n=nr,ucc.x=ux,ucc.y=uy,ucc=u))
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+#Universal Correlation Coefficient
+
+At the 2011 Joint Statistical Meetings, Nuo Xu of the University of Alabama at Birmingham presented a paper--coauthored with Xuan Huang also of UAB and Samuel Huang at the University of Cincinatti--in which a Universal Correlation Coefficient is defined and developed. This coefficient measures the degree of dependency (but not the form of dependency) for two discrete random variables.
+
+This R package implements the algorithm described in the paper presented at the above talk.
+
+##Example
+
+    library(ucc)
+
+We first grab 1,000 random `x` values between 0 and 1:
+
+    x <- runif(1000)
+
+We then make these points close to fitting on a curve (with a bit of randomly-generated noise):
+
+    dat <- data.frame(x=x,y=exp(x)*cos(2*pi*x) + runif(1000,-0.1,0.1))
+
+If you plot these data points, you'll see that there's an obvious (contrived) relationship, albeit not a linear relationship.
+
+![scatterplot](https://github.com/jackmaney/ucc/img/scatterplot.png)
+
+Predictably, the Pearson Correlation coefficient (ie the covariance divided by the product of standard deviations) does not reveal a linear relationship (due to randomness, exact values will vary, but it will probably be around 0.2)
+
+    cor(dat$x,dat$y)  # Low, even though there's obviously a relationship.
+
+The Universal Correlation Coefficient briefly described above measures the *degree* of dependency independent of the *form* of dependency (again, exact values will vary, but you should get UCC values above 0.9).
+
+    ucc.ucc(dat)    # Much higher.
+
+Check out the R documentation for more details about the algorithm.
